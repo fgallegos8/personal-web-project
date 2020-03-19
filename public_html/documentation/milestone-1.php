ca5758080a2c1cb54e51d9621e51bbf5ce3dedb9
Francisco Gallegos <fgallegos59@cnm.edu>
2020-03-19 12:23:18 -0600

    added content into milestone1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+    <h1>Francisco's Personal Web Page</h1>
+<body>
+<img src="James.jpg" alt="picture of James" class="center"/>
+<div class="center">
+    <ul>
+        <li>Name: James Smith</li>
+        <li>Age: 37</li>
+        <li>Occupation: Civil Engineer</li>
+        <li>Technology: Desktop, laptop, and a smartphone</li>
+        <li>Goals: Would like to own a jet pack and learn more about them so that he could safely use it to commute
+        to work.</li>
+        <li>Frustrations: Does not like being stuck in traffic and having to purchase gas every week.</li>
+
+        <li>James is a 37 year old civil engineer who is quite tech savvy. He is looking for an alternative method of
+        transportation but does not know what he should be looking for.</li>
+    </ul>
+    <h2>Purpose</h2>
+        <p>The purpose of this web-page is to give people an informed experience when considering purchasing a jet pack.</p>
+
+    <h2>Audience</h2>
+        <p>The target audience for this experience would be anyone who is interested in purchasing a jet pack
+        who has or does not have prior knowledge before purchase.</p>
+    <h2>Goal</h2>
+        <p>My goal is to inform people on jet packs and sell them one.</p>
+
+</body>
+
+</html>
