894d12135f61355bf2ef11af18cccfd163cf137b
Francisco Gallegos <fgallegos59@cnm.edu>
2020-03-27 20:37:04 -0600

    added content into milestone-2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8"/>
+    <title>Francisco's Personal Web Project</title>
+    </head>
+    <h1>Francisco's Personal Web Project</h1>
+    <body>
+        <h2>Section 1</h2>
+            <ul>
+                <li></li>
+            </ul>
+        <h2>Section 2</h2>
+            <ul>
+                <li></li>
+            </ul>
+        <h2>Section 3</h2>
+            <ul>
+                <li></li>
+            </ul>
+        <h2>Section 4</h2>
+            <ul>
+                <li></li>
+            </ul>
+
+    </body>
+</html>
