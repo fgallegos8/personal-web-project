1171d210d61b17b72f5c9a8f05c5de52a133ff02
Francisco Gallegos <fgallegos59@cnm.edu>
2020-03-31 21:16:45 -0600

    created an html document describing layout of web-page

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,19 +8,20 @@
     <body>
         <h2>Section 1</h2>
             <ul>
-                <li></li>
+                <li>In this section users will be greeted with an introduction to the web page. A brief
+                description of what they should expect from this website will be provided.</li>
             </ul>
         <h2>Section 2</h2>
             <ul>
-                <li></li>
+                <li>In this section a picture of a jet-pack wil be shown with some specs surrounding it.</li>
             </ul>
         <h2>Section 3</h2>
             <ul>
-                <li></li>
+                <li>In this section information regarding jet-packs and safety procedures will be provided.</li>
             </ul>
         <h2>Section 4</h2>
             <ul>
-                <li></li>
+                <li>Here contact information will be provided.it </li>
             </ul>
 
     </body>
